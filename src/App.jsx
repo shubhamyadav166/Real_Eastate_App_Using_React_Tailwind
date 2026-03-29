3ef5922af69dfca35510a054c952161ebb6f5ca4
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-03-29 21:38:18 +0530

    Layout, BrouserRouter created and router defined at main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from '../pages/Home'
-import About from '../pages/About'
-import SignIn from '../pages/SignIn'
-import SignUp from '../pages/SignUp'
-import Profile from '../pages/Profile'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Layout from './Layout'
+
 
 
 function App() {
     return (
 
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/sign-in" element={<SignIn />} />
-                <Route path="/sign-up" element={<SignUp />} />
 
-                <Route path="/profile" element={<Profile />} />
+        <Layout>
 
-            </Routes>
-        </BrowserRouter>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+
+            <Route path="/profile" element={<Profile />} />
+
+
+        </Layout>
 
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Layout from './Layout.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }, {
+        path: "sign-in",
+        element: <SignIn />
+      },
+      {
+        path: "sign-up",
+        element: <SignUp />
+      }
+    ]
+  },
+  {}
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
