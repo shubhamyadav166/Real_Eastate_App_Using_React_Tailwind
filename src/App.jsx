749ccc12c462420a39cdc55e07a81af08b84a606
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-03-29 17:45:12 +0530

    Real Estate project created using React and Tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+
+            <h1 className='bg-amber-600'>Lorem ipsum dolor sit amet.</h1>
+        </div>
+    )
+}
+
+export default App
