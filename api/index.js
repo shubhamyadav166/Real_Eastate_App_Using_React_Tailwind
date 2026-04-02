4f947ac1c990cb9bfd766762b9d83ed973835ba0
Shubham yadav <shubhamyadav1662000@gmail.com>
2026-04-02 17:56:55 +0530

    Error middleware created to get organized error

--- api/index.js
+++ api/index.js
@@ -15,3 +15,13 @@ app.use("/api/user",UserRouter)
 app.use("/api/auth",AuthRouter)
 app.listen(port,()=>console.log(`server is running on ${port} !!!`))
 
+app.use((err,req,res,next)=>{
+    const status=err.statusCode||500
+    const message=err.message||'Internal Server Error'
+    return res.status(status).json({
+        success:false,
+        status,
+        message
+    })
+
+})
